3bb2752716a530bb08956486ae87f1a0303bd5e1
twof <fabiobean2@gmail.com>
2016-06-25 16:17:41 -0700

    Fixed the form

--- app/src/main/java/com/example/terellbrown/checkyoself/InfoForm.java
+++ app/src/main/java/com/example/terellbrown/checkyoself/InfoForm.java
@@ -1,5 +1,7 @@
 package com.example.terellbrown.checkyoself;
 
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,24 +19,44 @@ public class InfoForm extends AppCompatActivity {
     public RadioButton sexFemale;
     public RadioButton sexOther;
     public CheckBox hasInsurance;
-    public Button searchButton;
+    public EditText password;
+    public EditText confirmPassword;
+    public Button registerButton;
+
+    private Context context;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_form);
-
+        context = this;
         name = (EditText) findViewById(R.id.etName);
         email = (EditText) findViewById(R.id.etEmail);
         sexMale = (RadioButton) findViewById(R.id.rbMale); 
         sexFemale = (RadioButton) findViewById(R.id.rbFemale); 
         sexOther = (RadioButton) findViewById(R.id.rbOther);
         hasInsurance = (CheckBox) findViewById(R.id.cbHasInsurace);
-        searchButton = (Button) findViewById(R.id.bSearch);
+        password = (EditText) findViewById(R.id.etPassword);
+        confirmPassword = (EditText) findViewById(R.id.etConfirmPassword);
+        registerButton = (Button) findViewById(R.id.bRegister);
 
-        searchButton.setOnClickListener(new View.OnClickListener() {
+        registerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                //Intent intent = new Intent(DoctorListActivity.class, )
+                String sexChecked;
+                if(sexMale.isActivated()){
+                    sexChecked = "male";
+                }else if(sexFemale.isActivated()){
+                    sexChecked = "female";
+                }else{
+                    sexChecked = "other";
+                }
+                Intent intent = new Intent(context, DoctorListActivity.class);
+                intent.putExtra(Constants.NAME,name.getText());
+                intent.putExtra(Constants.EMAIL,email.getText()); //Input validation?
+                intent.putExtra(Constants.HAS_INSURANCE,hasInsurance.isEnabled());
+                intent.putExtra(Constants.SEX, sexChecked);
+                intent.putExtra(Constants.PASSWORD, password.getText());
+                startActivity(intent);
             }
         });
     }
