88a0df3e227025f5c375d018c326f98e56165e7a
twof <fabiobean2@gmail.com>
2016-06-26 00:05:56 -0700

    Added more api stuff

--- app/src/main/java/com/example/terellbrown/checkyoself/API/PokitDokAPI.java
+++ app/src/main/java/com/example/terellbrown/checkyoself/API/PokitDokAPI.java
@@ -9,6 +9,8 @@ import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
+
 import android.util.Base64;
 
 /**
@@ -17,10 +19,13 @@ import android.util.Base64;
 public interface PokitDokAPI {
 
     @FormUrlEncoded
-    @POST("oath2/token")
+    @POST("oauth2/token")
     Call<Auth> authToken (@Header("Authorization") String authorization, @Field("grant_type") String grantType);
 
     @FormUrlEncoded
     @GET("/api/v4/providers")
-    Call<Provider> getProvider(@Header("Authorization") String authorization, @Field("zipcode") String zipcode, @Field("specialty") String specialty, @Field("radius") String radius);
+    Call<Provider> getProvidersByLocation(@Header("Authorization") String authorization, @Field("zipcode") String zipcode, @Field("specialty") String specialty, @Field("radius") String radius);
+
+    @GET("api/v4/providers/{NPI}")
+    Call<ProviderDetail> getProviderDetail(@Header("Authorization") String authorization, @Path("NPI") String npi);
 }
