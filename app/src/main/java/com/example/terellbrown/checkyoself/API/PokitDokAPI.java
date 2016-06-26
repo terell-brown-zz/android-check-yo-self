93966a4c1d278e66da0eba25dae99567e6f8812c
twof <fabiobean2@gmail.com>
2016-06-25 19:50:59 -0700

    Added api stuff

--- app/src/main/java/com/example/terellbrown/checkyoself/API/PokitDokAPI.java
+++ app/src/main/java/com/example/terellbrown/checkyoself/API/PokitDokAPI.java
@@ -1,12 +1,21 @@
 package com.example.terellbrown.checkyoself.API;
 
+import android.util.Base64;
+
 import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.Header;
 import retrofit2.http.POST;
+import android.util.Base64;
 
 /**
  * Created by fnord on 6/25/16.
  */
 public interface PokitDokAPI {
+
+    @FormUrlEncoded
     @POST("oath2/token")
-    Call<Auth> authToken ();
+    Call<Auth> authToken (@Header("Authorization") String authorization, @Field("grant_type") String grantType);
 }
